ac7b8ea560521c4a1dd3561ea44d512849132d8d
Scott Leggett <scott@sl.id.au>
2022-03-16 23:14:50 +0800

    chore: find the bug!

--- find.bug.sh
+++ find.bug.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+randrestart0() {
+	while true; do
+		sleep $(( RANDOM % 10 ))
+		docker-compose restart nats-0 2> /dev/null
+	done
+}
 randrestart1() {
 	while true; do
 		sleep $(( RANDOM % 10 ))
@@ -12,15 +18,15 @@ randrestart2() {
 		docker-compose restart nats-2 2> /dev/null
 	done
 }
+randrestart0 &
 randrestart1 &
 randrestart2 &
 
+trap 'kill $(jobs -p)' EXIT
+
 while true; do
-	docker-compose exec nats-box nats sub --server=nats-0 --queue=sshportalapi lagoon.sshportal.api > /dev/null &
-	sleep 1
+	docker-compose exec nats-box sh -c 'nats sub --server=nats-0 --queue=sshportalapi lagoon.sshportal.api > /dev/null & sleep 1; kill %1'
 	docker-compose logs nats-0 | grep -F '[RS+ $G lagoon.sshportal.api sshportalapi' && break
-	kill %3
 done
-kill %1 %2
 echo found the bug!
-docker-compose logs nats-0 | tail -n 30
+docker-compose logs nats-0 | tail -n 100
