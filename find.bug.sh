ce8b5af5728d81ad77da8ca9ffd97f83eb409bee
Scott Leggett <scott@sl.id.au>
2022-03-16 23:57:26 +0800

    fix: actually capture the buggy output

--- find.bug.sh
+++ find.bug.sh
@@ -26,7 +26,7 @@ trap 'kill $(jobs -p)' EXIT
 
 while true; do
 	docker-compose exec nats-box sh -c 'nats sub --server=nats-0 --queue=sshportalapi lagoon.sshportal.api > /dev/null & sleep 1; kill %1'
-	docker-compose logs nats-0 | grep -F '[RS+ $G lagoon.sshportal.api sshportalapi' && break
+	docker-compose logs nats-0 | grep -F -C 20 '[RS+ $G lagoon.sshportal.api sshportalapi' && break
 done
 echo found the bug!
-docker-compose logs nats-0 | tail -n 100
+echo inspect the logs via: docker-compose logs nats-0
